bc9f8d9b8746fc85bab48430a23ab1d9f3d1be47
austinerwin <austinerwinus@gmail.com>
2017-06-29 10:04:36 -0500

    Added incomplete totalPrice function

--- lib.js
+++ lib.js
@@ -19,13 +19,29 @@ const listedPrice =
         ? listing.price
         : 0
 
+const totalPrice =
+  (listing, cart) => {
+    let prices = []
+    for (let item of cart.items) {
+      prices.push(listedPrice(listing)(item))
+    }
+    return prices.reduce((a, b) => a + b)
+  }
 /**
  * transform carts into an array of { customer, total }
  */
 const calculateTotals =
   listings =>
     carts => {
-      // TODO
+
+      // carts.map((cart) =>
+      //     let items = cart.map((cart) => { cart.items })
+      //     {cart.customer, total}
+      // })
+      // let customers = carts.map((cart) => { cart.customer })
+      // let items = carts.map((cart) => { cart.items })
+      // let prices = items.map((item) => { listedPrice(listings)(item) })
+      // return { customer, total: totalPrice(cart) }
     }
 
 module.exports = {
